8b3b8b87f5279badfd82c47ab2732578e3e32934
Vladislav <lipatov.xex@gmail.com>
2022-12-05 14:55:27 +0700

    Removed unused files

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,4 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './index.css';
-import App from './App';
-
-render(() => <App />, document.getElementById('root'));
+render(() => <div>Test app</div>, document.getElementById("root"));
